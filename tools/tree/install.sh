376f489bdf768d93f93ba8d3cb1f9784db11a89b
itsp-technology <vivek1996.cloud@gmail.com>
2025-02-09 14:40:02 +0530

    add tree install sh script

---
+++ tools/tree/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Ensure the script is run as root
+if [ "$(id -u)" -ne 0 ]; then 
+  echo "❌ You should be a root user or use sudo to run this script."
+  exit 1
+fi 
+
+# Update package lists
+apt update -y 
+
+# Install Apache
+apt install -y tree
