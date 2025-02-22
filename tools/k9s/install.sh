19cdde6b88f482cb29393d01476e11bed47ce6b5
itsp-technology <vivek1996.cloud@gmail.com>
2025-02-22 16:36:10 +0530

    add curl

--- tools/k9s/install.sh
+++ tools/k9s/install.sh
@@ -3,6 +3,7 @@
 VERSION=$(curl -s https://github.com/derailed/k9s/releases | grep 'Release v' | head -1 | sed -e 's|<h1>||' -e 's|</h1>||' | awk '{print $2}')
 #curl -L https://github.com/derailed/k9s/releases/download/${VERSION}/k9s_Linux_x86_64.tar.gz | tar -xz
 #dnf install https://github.com/derailed/k9s/releases/download/${VERSION}/k9s_linux_amd64.rpm -y
-apt install https://github.com/derailed/k9s/releases/download/${VERSION}/k9s_Linux_amd64.tar.gz -y
+curl -LO "https://github.com/derailed/k9s/releases/download/${VERSION}/k9s_Linux_amd64.tar.gz" | tar -xz
+
 
 #https://github.com/derailed/k9s/releases/download/v0.40.5/k9s_Linux_amd64.tar.gz
