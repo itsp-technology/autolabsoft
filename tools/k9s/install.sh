2b6a94158dafe2cdfadf04923761866fcd475d8d
itsp-technology <vivek1996.cloud@gmail.com>
2025-02-22 16:38:21 +0530

    add move cmd to k9s dir

--- tools/k9s/install.sh
+++ tools/k9s/install.sh
@@ -4,6 +4,7 @@ VERSION=$(curl -s https://github.com/derailed/k9s/releases | grep 'Release v' | 
 #curl -L https://github.com/derailed/k9s/releases/download/${VERSION}/k9s_Linux_x86_64.tar.gz | tar -xz
 #dnf install https://github.com/derailed/k9s/releases/download/${VERSION}/k9s_linux_amd64.rpm -y
 curl -LO "https://github.com/derailed/k9s/releases/download/${VERSION}/k9s_Linux_amd64.tar.gz" | tar -xz
+sudo mv k9s /bin
 
 
 #https://github.com/derailed/k9s/releases/download/v0.40.5/k9s_Linux_amd64.tar.gz
