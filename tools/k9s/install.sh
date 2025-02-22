52ffd728afb8bb05ac4171d5a77a7a239f78d984
itsp-technology <vivek1996.cloud@gmail.com>
2025-02-22 16:58:48 +0530

    comment sudo

--- tools/k9s/install.sh
+++ tools/k9s/install.sh
@@ -4,7 +4,7 @@ VERSION=$(curl -s https://github.com/derailed/k9s/releases | grep 'Release v' | 
 #curl -L https://github.com/derailed/k9s/releases/download/${VERSION}/k9s_Linux_x86_64.tar.gz | tar -xz
 #dnf install https://github.com/derailed/k9s/releases/download/${VERSION}/k9s_linux_amd64.rpm -y
 curl -L "https://github.com/derailed/k9s/releases/download/${VERSION}/k9s_Linux_amd64.tar.gz" | tar -xz
-sudo mv k9s /bin
+#sudo mv k9s /bin
 
 # next hit the k9s inside sever machine you will get k9s console
 
